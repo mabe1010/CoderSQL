d4c6f63a2f2631c109afb4b36ef25bae661e0dc4
mabe1010 <85911194+mabe1010@users.noreply.github.com>
2021-12-21 23:57:08 -0300

    Update ScriptCompletoVasquez.sql

--- Desafio8/ScriptCompletoVasquez.sql
+++ Desafio8/ScriptCompletoVasquez.sql
@@ -544,7 +544,7 @@ CREATE PROCEDURE `eliminar y agregar valores en dimempleados`(IN IdEmpleado INT)
 BEGIN
 INSERT INTO colegio_pablo_neruda.dimempleados (Nombre, Apellido, DNI, Edad, Correo, Telefono, Direccion, EstadoCivil, Cargo, FechaIngreso, FechaEgreso)
 VALUES ('Ana', 'Soledad', 96545263, 26, 'anasole@gmail.com', 1154896578, 'Lavalleja 105', 'Soltera', 'Secretaria Control Estudio', '2021-11-14', NULL);
-DELETE FROM colegio_pablo_neruda.dimepleados
+DELETE FROM colegio_pablo_neruda.dimempleados
 WHERE IdEmpleado = IdEmpleado;
 END//
 DELIMITER ;
