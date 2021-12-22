b3000bd577b54caac35e118ed93933eff5721a4f
mabe1010 <85911194+mabe1010@users.noreply.github.com>
2021-12-21 23:57:41 -0300

    Update Stored2Vasquez.sql

--- Desafio8/Stored2Vasquez.sql
+++ Desafio8/Stored2Vasquez.sql
@@ -2,6 +2,6 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `eliminar y agregar valores en dimem
 BEGIN
 INSERT INTO colegio_pablo_neruda.dimempleados (Nombre, Apellido, DNI, Edad, Correo, Telefono, Direccion, EstadoCivil, Cargo, FechaIngreso, FechaEgreso)
 VALUES ('Ana', 'Soledad', 96545263, 26, 'anasole@gmail.com', 1154896578, 'Lavalleja 105', 'Soltera', 'Secretaria Control Estudio', '2021-11-14', NULL);
-DELETE FROM colegio_pablo_neruda.dimepleados
+DELETE FROM colegio_pablo_neruda.dimempleados
 WHERE IdEmpleado = IdEmpleado;
 END
